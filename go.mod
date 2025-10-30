18ff982d09e4247af70d934cbbf84f4d36c10c34
jhonnydsl <jhonny.s.l.12345@gmail.com>
2025-10-30 04:03:22 -0300

    feat: initia WebRTC setup
    - Installed the latest version of the `pion/webrtc` library
    - Created `web_rtc.go` in the `services` package
    - Defined basic structs (`Participant`, `RTCSession`, `RTCService`)
    - Implemented main functions:
    	- `CreateSession` (host starts the meeting and generates offer
    	  SDP)
    	- `JoinSession` (participant joins the meeting and redistributes
    	  tracks)
    	- `LeaveSession` (participant leaves, closes PeerConnection)
    - Set up `OnIceCandite` and `OnTrack` callbacks for future WebSockets
      integration
    - Properly initialized `map[int]*Participant` to avoid panics

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -34,6 +35,22 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pion/datachannel v1.5.10 // indirect
+	github.com/pion/dtls/v3 v3.0.7 // indirect
+	github.com/pion/ice/v4 v4.0.10 // indirect
+	github.com/pion/interceptor v0.1.41 // indirect
+	github.com/pion/logging v0.2.4 // indirect
+	github.com/pion/mdns/v2 v2.0.7 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.15 // indirect
+	github.com/pion/rtp v1.8.23 // indirect
+	github.com/pion/sctp v1.8.40 // indirect
+	github.com/pion/sdp/v3 v3.0.16 // indirect
+	github.com/pion/srtp/v3 v3.0.8 // indirect
+	github.com/pion/stun/v3 v3.0.0 // indirect
+	github.com/pion/transport/v3 v3.0.8 // indirect
+	github.com/pion/turn/v4 v4.1.1 // indirect
+	github.com/pion/webrtc/v4 v4.1.6 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
@@ -42,6 +59,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	github.com/urfave/cli/v2 v2.27.7 // indirect
+	github.com/wlynxg/anet v0.0.5 // indirect
 	github.com/xrash/smetrics v0.0.0-20250705151800-55b8f293f342 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	golang.org/x/arch v0.20.0 // indirect
---
+++ src/services/web_rtc.go
@@ -0,0 +1,166 @@
+package services
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/pion/webrtc/v4"
+)
+type Participant struct {
+	ID int
+	PC *webrtc.PeerConnection
+}
+
+type RTCSession struct {
+	PeerConnection *webrtc.PeerConnection
+	Participants map[int]*Participant
+	Mutex sync.Mutex
+}
+
+type RTCService struct {
+	Sessions map[int]*RTCSession
+	Mutex sync.Mutex
+}
+
+func NewRTCService() *RTCService {
+	return &RTCService{
+		Sessions: make(map[int]*RTCSession),
+	}
+}
+
+func (service *RTCService) CreateSession(meetingID int) (string, error) {
+	service.Mutex.Lock()
+	defer service.Mutex.Unlock()
+
+	pc, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		return "", err
+	}
+
+	session := &RTCSession{
+		PeerConnection: pc,
+		Participants: make(map[int]*Participant),
+	}
+	service.Sessions[meetingID] = session
+
+	_, err = pc.CreateDataChannel("chat", nil)
+	if err != nil {
+		return "", err
+	}
+
+	offer, err := pc.CreateOffer(nil)
+	if err != nil {
+		return "", err
+	}
+
+	err = pc.SetLocalDescription(offer)
+	if err != nil {
+		return "", err
+	}
+
+	pc.OnICECandidate(func(c *webrtc.ICECandidate) {
+		if c != nil {
+			// codigo que envia json via websocket
+		}
+	})
+
+	return offer.SDP, nil
+}
+
+func (service *RTCService) JoinSession(meetingID, userID int, offerSDP string) (string, error) {
+	service.Mutex.Lock()
+	session, ok := service.Sessions[meetingID]
+	service.Mutex.Unlock()
+	if !ok {
+		return "", fmt.Errorf("session not found")
+	}
+
+	pc, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		return "", err
+	}
+
+	session.Mutex.Lock()
+	defer session.Mutex.Unlock()
+
+	newParticipant := &Participant{ID: userID, PC: pc}
+	session.Participants[userID] = newParticipant
+
+	pc.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
+		session.Mutex.Lock()
+
+		for _, other := range session.Participants {
+			if other.ID == userID {
+				continue
+			}
+
+			localTrack, err := webrtc.NewTrackLocalStaticRTP(track.Codec().RTPCodecCapability, track.ID(), track.StreamID())
+			if err != nil {
+				fmt.Println("error creating localStrack:", err)
+				continue
+			}
+
+			other.PC.AddTrack(localTrack)
+
+			go func() {
+				buf := make([]byte, 1500)
+				for {
+					n, _, readErr := track.Read(buf)
+					if readErr != nil {
+						return 
+					}
+					_, writeErr := localTrack.Write(buf[:n])
+					if writeErr != nil {
+						return 
+					}
+				}
+			}()
+		}
+		session.Mutex.Unlock()
+	})
+
+	err = pc.SetRemoteDescription(webrtc.SessionDescription{
+		Type: webrtc.SDPTypeOffer,
+		SDP: offerSDP,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	answer, err := pc.CreateAnswer(nil)
+	if err != nil {
+		return "", err
+	}
+
+	err = pc.SetLocalDescription(answer)
+	if err != nil {
+		return "", err
+	}
+
+	pc.OnICECandidate(func(c *webrtc.ICECandidate) {
+		if c != nil {
+			// codigo que envia json via websocket
+		}
+	})
+
+	return answer.SDP, nil
+}
+
+func (service *RTCService) LeaveSession(meetingID, userID int) {
+	service.Mutex.Lock()
+	session, ok := service.Sessions[meetingID]
+	service.Mutex.Unlock()
+	if !ok {
+		return 
+	}
+
+	session.Mutex.Lock()
+	defer session.Mutex.Unlock()
+
+	if p, exists := session.Participants[userID]; exists {
+		p.PC.Close()
+		delete(session.Participants, userID)
+	}
+}
+
+//func AddIceCandidate(meetingID int, candidate string, userID int) error {}
