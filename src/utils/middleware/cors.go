e6dc1596c481b25df8e529c348af6db9f9317f41
jhonnydsl <jhonny.s.l.12345@gmail.com>
2025-11-11 22:28:07 -0300

    update cors

--- src/utils/middleware/cors.go
+++ src/utils/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
